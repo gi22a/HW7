3fb049c46574aae3601550eb98eb907c662b366f
ilya <arhipov.a2cat@mail.ru>
2023-06-13 10:44:54 +0300

    zadacha50

---
+++ zadacha50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void CraeteArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 50);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+string SerchNumber(int[,] arr, int number)
+{
+    string mesSerch = "такое число есть";
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (number == arr[i, j])
+            {
+                return mesSerch;
+            }
+        }
+    }
+    return mesSerch = "такого числа нет";
+}
+Console.Write("Введите число M: ");
+int row = InputNumber();
+Console.Write("Введите число N: ");
+int colums = InputNumber();
+
+int[,] array = new int[row, colums];
+CraeteArray(array);
+PrintArray(array);
+
+Console.Write("Введите число для поиска: ");
+int numberSearch = InputNumber();
+System.Console.WriteLine(SerchNumber(array, numberSearch));
